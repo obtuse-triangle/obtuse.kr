c4e40c8cc9b187f0229f6d1e0af1f72fcbf3b97f
obtuse-triangle <me@obtuse.kr>
2024-07-10 08:54:18 +0900

    example blog

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://obtuse.kr",
+  integrations: [tailwind(), sitemap(), mdx()],
+  server: {
+    host: "0.0.0.0"
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
