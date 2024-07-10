77da14983fb259d00b3ee6d319b3dd5b55f3a43d
obtuse-triangle <me@obtuse.kr>
2024-07-10 20:22:46 +0900

    🔧 tailwindcss typography darkmode configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,55 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    colors: {
+      primaryColor: "#E3D4FF",
+      secondaryColor: "#433B4E",
+      white: "#fff",
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.800"),
+            a: {
+              color: theme("colors.blue.600"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+              },
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme("colors.white"),
+            a: {
+              color: theme("colors.primaryColor"),
+              "&:hover": {
+                color: theme("colors.secondaryColor"),
+              },
+            },
+            h1: {
+              color: theme("colors.gray.100"),
+            },
+            h2: {
+              color: theme("colors.gray.100"),
+            },
+            h3: {
+              color: theme("colors.gray.100"),
+            },
+            p: {
+              color: theme("colors.gray.300"),
+            },
+            strong: {
+              color: theme("colors.gray.100"),
+            },
+            code: {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
