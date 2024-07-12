53ccaa70d5223302c0dfc25eac5b2572a38b63a3
obtuse-triangle <me@obtuse.kr>
2024-07-13 03:48:17 +0900

    💥 신고기능 추가 - 댓글 완성

--- public/post/comment.js
+++ public/post/comment.js
@@ -73,4 +73,43 @@ document.querySelectorAll("a.commentReply").forEach((elm) => {
   });
 });
 
-//report comment
+//toggle report form
+document.querySelectorAll("a.commentReport").forEach((elm) => {
+  elm.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const commentReportForm =
+      e.target.parentElement.parentElement.parentElement.querySelector(".reportForm");
+    commentReportForm.classList.toggle("hidden");
+  });
+});
+
+//submit report form
+const reportForms = document.querySelectorAll(".reportForm");
+reportForms.forEach((reportForm) => {
+  reportForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const body = {
+      content: e.target.content.value,
+    };
+    fetch(
+      `https://strapi.obtuse.kr/api/comments/api::article.article:${e.target.postID.value}/comment/${e.target.commentID.value}/report-abuse`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer bc31b59f4cf0df8dda5c4dceb3792434a3e9ec3620333266b01d4edb7207dc1654cf9c84bad0c03a60caf495a86c1399ebe9dbda54c6e6e1ea8a770a7b131c41d153f3b245201f7bebdc8c69cf31a63af100876534fbbdd52892c71ff3225355cf85217d78c7cde1f7103041e205cb2ceb2861c918d138060db197c5a73dea1e`,
+        },
+        body: JSON.stringify(body),
+      }
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          e.target.parentElement.parentElement.innerHTML =
+            "신고가 접수되었습니다. 댓글 삭제는 검토 후 이루어집니다.";
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  });
+});
